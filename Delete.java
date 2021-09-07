05e42c5252b2d71b385fd7b27e2eca2e72006b18
Angsuman Chakraborty <angsuman@taragana.com>
2021-09-07 13:05:15 +0530

    Added 2 blank lines to improve readability

--- Delete.java
+++ Delete.java
@@ -1,9 +1,12 @@
 import java.io.File;
+
 public class Delete {
+  
   /** Recursively delete multiple files and directories specified in command line */
   public static void main(String paths[]) {
     for(String path : paths) if(path != null) delete(new File(path));
   }
+  
   /** Recursively delete Directory and File on best effort basis */
   public static void delete(File file) {
     if(file != null && file.exists()) {
